e74b0d267117d3eb2fab3ddd1349cab2d718e6e5
tuna2134 <contact@tuna2134.dev>
2024-06-05 10:52:40 +0900

    fox

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::fs::File;
 use ac_ffmpeg::{
-    format::{io::IO, demuxer::Demuxer},
-    codec::{audio::{AudioResampler, AudioDecoder, AudioEncoder}, Decoder},
+    codec::{
+        audio::{AudioDecoder, AudioResampler},
+        Decoder,
+    },
+    format::{demuxer::Demuxer, io::IO},
 };
+use std::fs::File;
 
 fn main() -> anyhow::Result<()> {
     let mut wav_file = File::open("tada-81529.mp3")?;
@@ -19,12 +22,10 @@ fn main() -> anyhow::Result<()> {
         .enumerate()
         .find(|(_, params)| params.is_audio_codec())
         .ok_or_else(|| anyhow::anyhow!("No audio stream found"))?;
-    let codec_params = binding.as_audio_codec_parameters()
-        .unwrap();
-    
-    let mut decoder = AudioDecoder::from_codec_parameters(&codec_params)?
-        .build()?;
-    
+    let codec_params = binding.as_audio_codec_parameters().unwrap();
+
+    let mut decoder = AudioDecoder::from_codec_parameters(&codec_params)?.build()?;
+
     let mut resampler = AudioResampler::builder()
         .source_channel_layout(codec_params.channel_layout().to_owned())
         .source_sample_format(codec_params.sample_format())
@@ -34,8 +35,8 @@ fn main() -> anyhow::Result<()> {
         .target_sample_rate(48000)
         .build()?;
     let mut output = File::create("output.wav")?;
-    let mut io_output = IO::from_seekable_read_stream(&mut output);
-    
+    let _io_output = IO::from_seekable_read_stream(&mut output);
+
     while let Some(packet) = demuxer.take()? {
         if packet.stream_index() == index {
             decoder.push(packet)?;
@@ -43,9 +44,7 @@ fn main() -> anyhow::Result<()> {
             while let Some(frame) = decoder.take()? {
                 resampler.push(frame)?;
 
-                while let Some(frame) = resampler.take()? {
-
-                }
+                while let Some(_frame) = resampler.take()? {}
             }
         }
     }
